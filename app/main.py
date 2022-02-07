baa18f32be7d1d616095016a6632af5369155020
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2022-02-07 11:32:22 -0800

    refactor: serves html files now without errors removed templates dir consolidated all static files into static dirs mounted static dir and add dependencies file for future  use

--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from app.routers import auth, user
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, FileResponse
+from app.routers import auth, user
+
 from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="app/static", html=True), name="static")
+
 app.include_router(user.router)
 app.include_router(auth.router)
+
+'''
+USE JINJA, SEE STACK OVERFLOW SOLUTION
+
+'''
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,30 +1,31 @@
 from fastapi import status, HTTPException, APIRouter, Query
 from sys import prefix
 from fastapi import Request
-from fastapi.responses import HTMLResponse
-
-
+from fastapi.responses import HTMLResponse, FileResponse
 
 # prefix = adds the string to the beginning of each path op
 # tags = is for the docs website for this api, it groups these path ops under 'Users' in the docs
 router = APIRouter(
     prefix="/user", 
-    tags=['User'] 
+    tags=['User']
 )
 
 # path op GETs the create user page to render
-@router.get("/create", response_class=HTMLResponse) 
-async def get_create_page():
-    return """
-    <html>
-        <head>
-            <title>Some HTML in here</title>
-        </head>
-        <body>
-            <h1>Look ma! HTML!</h1>
-        </body>
-    </html>
-    """
+# @router.get("/create", response_class=HTMLResponse) 
+# async def get_create_page():
+#     return """
+#     <html>
+#         <head>
+#             <title>Some HTML in here</title>
+#         </head>
+#         <body>
+#             <h1>Look ma! HTML!</h1>
+#         </body>
+#     </html>
+#     """
+@router.get("/create", response_class=FileResponse) 
+def get_create_page():
+    return 'app/static/template_base.html'
 
 # path op when user is done filling out form and hits 'done' button
 @router.post("/create")
