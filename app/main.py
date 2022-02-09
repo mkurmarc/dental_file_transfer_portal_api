7890569345a2a8e5e8241aa999b03c6d358bf9b8
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2022-02-08 19:53:00 -0800

    del(templates): deleted templated dir and its contents

--- app/main.py
+++ app/main.py
@@ -2,13 +2,21 @@ from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, FileResponse
 from app.routers import auth, user
-
 from fastapi.templating import Jinja2Templates
+from pathlib import Path
+
+BASE_PATH = Path(__file__).resolve().parent
+TEMPLATES = Jinja2Templates(directory="app/static")
 
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="app/static", html=True), name="static")
+app.mount(
+    "/static", 
+    StaticFiles(directory="app/static"),
+    name="static"
+)
+
 
 app.include_router(user.router)
 app.include_router(auth.router)
--- app/routers/user.py
+++ app/routers/user.py
@@ -2,6 +2,7 @@ from fastapi import status, HTTPException, APIRouter, Query
 from sys import prefix
 from fastapi import Request
 from fastapi.responses import HTMLResponse, FileResponse
+# from app.main import templates
 
 # prefix = adds the string to the beginning of each path op
 # tags = is for the docs website for this api, it groups these path ops under 'Users' in the docs
@@ -23,9 +24,11 @@ router = APIRouter(
 #         </body>
 #     </html>
 #     """
-@router.get("/create", response_class=FileResponse) 
-def get_create_page():
-    return 'app/static/template_base.html'
+@router.get("/create") 
+async def get_create_page(request: Request):
+    return templates.TemplateResponse(
+        "app/static/template_base.html", {"request": request}
+    )
 
 # path op when user is done filling out form and hits 'done' button
 @router.post("/create")
