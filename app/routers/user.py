8b40598b546652d13512410feeca1ca3afa6d87e
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2022-02-08 21:26:07 -0800

    style(login): added html inputs and did basic styling to the html content in gen_login()

--- app/routers/user.py
+++ app/routers/user.py
@@ -3,7 +3,7 @@ from sys import prefix
 from fastapi import Request
 from fastapi.responses import HTMLResponse, FileResponse
 # from app.main import templates
-from app.static.html_generator import generate_create_user
+from app.static.html_generator import gen_create_user, gen_login
 
 # prefix = adds the string to the beginning of each path op
 # tags = is for the docs website for this api, it groups these path ops under 'Users' in the docs
@@ -12,18 +12,19 @@ router = APIRouter(
     tags=['User']
 )
 
+
+@router.get("/login", response_class=HTMLResponse) 
+async def get_login_page():
+    return gen_login()
+
+
 # path op GETs the create user page to render
 @router.get("/create", response_class=HTMLResponse) 
 async def get_create_page():
-    return generate_create_user()
+    return gen_create_user()
 
-# @router.get("/create") 
-# async def get_create_page(request: Request):
-#     return templates.TemplateResponse(
-#         "app/static/template_base.html", {"request": request}
-#     )
 
 # path op when user is done filling out form and hits 'done' button
 @router.post("/create")
-def create_user(username: str = Query(None, max_length=20)):
+def create_user(username: str = Query(None, max_length=20), password: str = None):
     return 
--- app/static/html_generator.py
+++ app/static/html_generator.py
@@ -1,6 +1,6 @@
 from fastapi.responses import HTMLResponse
 
-def generate_create_user():
+def gen_create_user():
     html_content = """
     <html>
         <head>
@@ -14,6 +14,60 @@ def generate_create_user():
         <body>
             <h1>Look ma! HTML!</h1>
         </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+def gen_login():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+            <style>
+                body {
+                    background: #1D1E1D;
+                    display: flex;
+                    justify-content: center;
+                }
+
+                .container {
+                    width: 35%;
+                    height: 35%;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center; 
+                    gap: 10px;
+                }
+
+                .items {
+                    width: 100%;
+                }
+
+                h1 {
+                    color: silver;
+                    align-self: center;
+                }
+
+                button {
+                    border-radius: 8px;
+                }
+
+
+            </style>
+        </head>
+
+        <body>
+            
+            <div class="container">
+                <h1>Login</h1>
+                <input class="items" type="email" name="email" placeholder="your@email.com" size="30" required>
+                <input class="items" type="password" name="password" placeholder="password" size="20" required>
+                <button class="items" type="button" name="login_b">Login</button>
+            </div>
+
+        </body>
+
     </html>
     """
     return HTMLResponse(content=html_content, status_code=200)
