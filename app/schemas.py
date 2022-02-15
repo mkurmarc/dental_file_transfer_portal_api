027420585e15bd29654e7e190b4af3d20abb30af
mkurmarc <51379562+mkurmarc@users.noreply.github.com>
2022-02-14 23:33:12 -0800

    build(schemas): 3 pydantic models for incoming data validation

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,28 @@
-from pydantic import BaseModel, EmailStr
+import email
+from pydantic import BaseModel, EmailStr, FilePath, SecretStr
 
-# user routes
+'''auth routes''' 
+# incoming POST - user login info
 class UserLogin(BaseModel):
     email: EmailStr
-    password: str
+    password: SecretStr
+
+
+'''user routes'''
+# incoming POST - create user info
+class CreateUser(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    confirm_email: EmailStr
+    password: SecretStr
+    confirm_password: SecretStr
+    isAccepted: bool
+
+'''upload_files routes'''
+# incoming POST - files and names assoc
+class Upload(BaseModel):
+    first_name: str
+    last_name: str
+    # custom data type for files
 
