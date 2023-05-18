25bb5a7c31c3070c86f2df59b472a7f84af1d45e
mkurmarc <marc.rios1712@gmail.com>
2023-05-17 17:15:24 -0700

    refactor: changed admin routes to return JSON

--- app/internal/admin.py
+++ app/internal/admin.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, File, Depends
-from fastapi.responses import HTMLResponse
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session 
 from app import database, schemas, models, utils, oauth2
 
@@ -9,7 +9,7 @@ router = APIRouter(
 )
 
 # routes for admin only users; retrieves files from DB and serves them with html, use Jinja template
-@router.get("/", response_class=HTMLResponse)
+@router.get("/", response_class=JSONResponse)
 async def get_admin_dashboard(admin_user: int = Depends(oauth2.get_admin_user),
 db: Session = Depends(database.get_db), limit: int = 10):       
 
@@ -17,9 +17,9 @@ db: Session = Depends(database.get_db), limit: int = 10):
     # load 10 most recent files uploaded
     files = db.query(models.File).order_by(models.File.created_at).limit(limit) 
     
-    return gen_admin_dashboard() # return render_template("/login.html", files)
+    return {"data": "payload"}
 
 
-@router.post("/")
+@router.post("/", response_class=JSONResponse)
 async def update_admin(admin_user: int = Depends(oauth2.get_admin_user)):
     return {"message": "Admin getting schwifty"}
